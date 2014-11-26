06579a0b0a4a971d0faa5a66fca5afc6cca4c934
Erik Ashcraft <eriktashcraft@hotmail.com>
2014-11-26 10:17:30 -0600

    Create ColorManager.java

---
+++ abs_editor/editor/coor_manager/ColorManager.java
@@ -0,0 +1,33 @@
+package abs_editor.editor.color_manager;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.eclipse.jface.text.source.ISharedTextColors;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.widgets.Display;
+
+public class ColorManager implements ISharedTextColors{
+
+	@SuppressWarnings("rawtypes")
+	protected Map fColorTable = new HashMap(10);
+	
+	public void dispose() {
+		@SuppressWarnings("rawtypes")
+		Iterator e = fColorTable.values().iterator();
+		while (e.hasNext())
+			 ((Color) e.next()).dispose();
+	}
+	@SuppressWarnings("unchecked")
+	public Color getColor(RGB rgb) {
+		Color color = (Color) fColorTable.get(rgb);
+		if (color == null) {
+			color = new Color(Display.getCurrent(), rgb);
+			fColorTable.put(rgb, color);
+		}
+		return color;
+	}
+
+}
