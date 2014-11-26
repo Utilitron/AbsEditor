3af96c69c940c7d8b96a3242b2776d304c326601
Erik Ashcraft <eriktashcraft@hotmail.com>
2014-11-26 10:17:00 -0600

    Create WhiteSpaceDetector.java

---
+++ abs_editor/editor/WhiteSpaceDetector.java
@@ -0,0 +1,10 @@
+package abs_editor.editor;
+
+import org.eclipse.jface.text.rules.IWhitespaceDetector;
+
+public class WhitespaceDetector implements IWhitespaceDetector {
+
+	public boolean isWhitespace(char c) {
+		return (c == ' ' || c == '\t' || c == '\n' || c == '\r');
+	}
+}
