ec51e3108c676996fc4daea21511eff386ccf28b
Erik Ashcraft <eriktashcraft@hotmail.com>
2014-11-26 10:23:50 -0600

    Create ISourceListener.java

---
+++ abs_editor/source/model/ISourceListener.java
@@ -0,0 +1,12 @@
+package abs_editor.source.model;
+
+import abs_editor.source.event.AbsChangeEvent;
+
+/**
+ * 
+ */
+public interface ISourceListener {
+
+    void absChanged(AbsChangeEvent event);
+
+}
